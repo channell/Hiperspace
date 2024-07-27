17f0910b7a9ddbced1c61ce375f1cf9c896d7a2b
Stephen Channell <steve.channell@cepheis.com>
2024-07-27 17:13:06 +0100

    Updated invoice sample

---
+++ examples/Invoice/ERPSpace.cs
@@ -0,0 +1,54 @@
+﻿using ERP.Finance;
+using ERP.Location;
+using ERP.Products;
+using ERP.Sales;
+using Hiperspace;
+
+namespace ERP
+{
+    public partial class ERPSpace
+    {
+        private static Horizon[] constraints = new Horizon[]
+        {
+            new Horizon<Address>        (i => i.Valid == true),
+            new Horizon<Country>        (i => i.Valid == true),
+            new Horizon<Tarrif>         (i => i.Valid == true),
+            new Horizon<Tax>            (i => i.Valid == true),
+            new Horizon<Shipping>       (i => i.Valid == true),
+            new Horizon<Product>        (i => i.Valid == true),
+            new Horizon<ProductPrice>   (i => i.Valid == true),
+            new Horizon<Shipping>       (i => i.Valid == true),
+            new Horizon<Salesperson>    (i => i.Valid == true),
+            new Horizon<Order>          (i => i.Valid == true),
+        };
+
+        public ERPSpace(HiperSpace space, DateTime? AsAt = null, DateTime? DeltaFrom = null) 
+            : this(space, constraints, AsAt, DeltaFrom)        
+        { }
+    }
+    internal static class ERPFunctions
+    {
+        public static bool Changed<V>(V? v) where V : ElementVersion<V>, new()
+        {
+            if (v != null)
+            {
+                var c = v?.SetSpace?.Get(v);
+                if (c != null && v != c) return true;
+            }
+            return false;
+        }
+        public static bool ChangedLines(Order order)
+        {
+            var c = order.SetSpace?.Get(order);
+            if (c != null)
+            {
+                var lines = c.Lines;
+                if (lines != null && order.Lines != null)
+                { 
+                    return lines.SequenceEqual(order.Lines);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ examples/Invoice/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using ERP.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Sales
+{
+    public partial class OrderInvoice
+    {
+        /// <summary>
+        /// Snapshot the order financials
+        /// </summary>
+        /// <param name="order"></param>
+        public OrderInvoice(Order order) 
+        { 
+            Commission = order.Commission;
+            Cost = order.Cost;
+            Total = order.Total;
+            Profit = order.Profit;
+            SalesTax = order.SalesTax;
+            ShippingCost = order.ShippingCost;
+            TariffTax = order.TariffTax;
+            Total = order.Total;
+            TaxDate = DateTime.UtcNow.Date;
+        }
+    }
+}
